978a397d74a1072284e0d8a20c14c166ce243a1a
Patricie <patricie.sm@gmail.com>
2024-10-02 14:47:38 +0200

    refactor: fix build

--- src/hooks/tokenomics/useTotalSupply.ts
+++
@@ -1,6 +0,0 @@
-import getTotalSupply from 'api/tokenomics/getTotalSupply'
-import useSWRImmutable from 'swr/immutable'
-
-export default function useTotalSupply() {
-  return useSWRImmutable('tokenomics/totalSupply', async () => getTotalSupply())
-}
